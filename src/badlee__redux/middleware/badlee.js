e45f8e633ae6b8ee3e1fb34bd4918b64b9b737ad
Rahul Garg <rahul_garg08@yahoo.com>
2017-08-21 01:24:20 +0530

    changed image to random only

--- src/badlee__redux/middleware/badlee.js
+++ src/badlee__redux/middleware/badlee.js
@@ -5,10 +5,16 @@ import * as actionCreators from "../action_creators";
 async function saveBadlee(store, next, action) {
   try {
     await store.dispatch(actionCreators.startLoading());
+    const randomImages = [
+      "https://s-media-cache-ak0.pinimg.com/originals/3b/07/6c/3b076c58a2428b5fa6352c3832198fd4.jpg",
+      "https://pbs.twimg.com/profile_images/684388992674996224/zYQDMpIo.png",
+      "http://pa1.narvii.com/5765/4e02a449452fa70fe41b29219d08c484a9e6c436_hq.gif",
+      "http://orig05.deviantart.net/0b1e/f/2011/220/5/8/grimmjow__rape_face_by_kittywillcutyou-d45x00w.jpg"
+    ];
     const data = {
       media: action.media
         ? action.media
-        : "https://s-media-cache-ak0.pinimg.com/originals/3b/07/6c/3b076c58a2428b5fa6352c3832198fd4.jpg",
+        : randomImages[Math.floor(Math.random() * 4)],
       description: action.description,
       ip: action.ip,
       location: action.location,

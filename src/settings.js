e7dc6cb833f39afd3fa7061dc5f6ca644ff439d6
Rahul Garg <rahul_garg08@yahoo.com>
2017-09-03 03:46:34 +0530

    removed useless files

--- src/settings.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  server: "http://localhost:8000",
-  session_key: "@AsyncStorageSTK:sessonId",
-  endpoints: {},
-  request_headers: {
-    Accept: "application/json",
-    "Content-Type": "application/json"
-  }
-};

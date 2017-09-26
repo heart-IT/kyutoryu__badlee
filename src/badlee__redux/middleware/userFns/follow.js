4eeb2d62c1b00633f3141fa562a9513ec176fe99
Rahul Garg <rahul_garg08@yahoo.com>
2017-09-27 01:16:37 +0530

    added live update to state in follow

--- src/badlee__redux/middleware/userFns/follow.js
+++ src/badlee__redux/middleware/userFns/follow.js
@@ -16,7 +16,8 @@ import * as actionCreators from '../../action_creators';
 
 export async function unFollowUser(store, next, action) {
   try {
-    await store.dispatch(actionCreators.startLoading());
+    action.userID = action.id;
+    next(action);
     let jollyroger = await AsyncStorage.getItem("jollyroger");
     var unFollowReq = await fetch(
       `http://mri2189.badlee.com/follow.php?userid=${action.id}`,
@@ -27,20 +28,19 @@ export async function unFollowUser(store, next, action) {
         }
       }
     );
-    if (unFollowReq.status === 200 && unFollowReq.ok) {
-      action.userID = action.id;
-      next(action);
+    if (!unFollowReq.status === 200 || !unFollowReq.ok) {
+      store.dispatch(actionCreators.followUser(action.id));
     }
   } catch (err) {
     console.log(err);
   } finally {
-    await store.dispatch(actionCreators.finishLoading());
   }
 }
 
 export async function followUser(store, next, action) {
   try {
-    await store.dispatch(actionCreators.startLoading());
+    action.userID = action.id;
+    next(action);
     let jollyroger = await AsyncStorage.getItem("jollyroger");
     var followReq = await fetch(
       `http://mri2189.badlee.com/follow.php?userid=${action.id}`,
@@ -51,13 +51,11 @@ export async function followUser(store, next, action) {
         }
       }
     );
-    if (followReq.status === 200 && followReq.ok) {
-      action.userID = action.id;
-      next(action);
+    if (!followReq.status === 200 || !followReq.ok) {
+      store.dispatch(actionCreators.unFollowUser(action.id));
     }
   } catch (err) {
     console.log(err);
   } finally {
-    await store.dispatch(actionCreators.finishLoading());
   }
 }

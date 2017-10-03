2a1da41ae81e3bec58fdec1a9facce1a331a0a69
Rahul Garg <rahul_garg08@yahoo.com>
2017-10-04 01:59:12 +0530

    single badlee page opening good

--- src/containers/Authenticated/SingleBadlee.js
+++ src/containers/Authenticated/SingleBadlee.js
@@ -16,7 +16,6 @@ import {
     Container,
     Content,
     Header,
-    Icon as IconX,
     Left,
     Right,
     StyleProvider,
@@ -32,10 +31,51 @@ import { connect } from 'react-redux';
 import * as actionCreators from '../../badlee__redux/action_creators';
 import Icon from '../../components/Icon';
 import getTheme from '../../theme/components';
+import User from './User';
 
 ("use strict");
 
 class SingleBadlee extends Component {
+  constructor(props) {
+    super(props);
+    this.onBackPress = this.onBackPress.bind(this);
+    this.onClickUser = this._onClickUser.bind(this);
+    this.onClickLike = this._onClickLike.bind(this);
+    this.onClickUnlike = this._onClickUnlike.bind(this);
+    this.onClickWish = this._onClickWish.bind(this);
+    this.onClickUnwish = this._onClickUnwish.bind(this);
+    this.onClickComment = this._onClickComment.bind(this);
+    this.onClickDelete = this._onClickDelete.bind(this);
+  }
+  onBackPress() {
+    this.props.goBack();
+  }
+  _onClickUser() {
+    requestAnimationFrame(() => {
+      this.props.showUserPage(this.props.badlee.get("user"), {
+        navigator: this.props.navigator,
+        component: User
+      });
+    });
+  }
+  _onClickLike() {
+    this.props.onClickLike(this.props.badlee.get("id"));
+  }
+  _onClickUnlike() {
+    this.props.onClickUnlike(this.props.badlee.get("id"));
+  }
+  _onClickWish() {
+    this.props.onClickWish(this.props.badlee.get("id"));
+  }
+  _onClickUnwish() {
+    this.props.onClickUnwish(this.props.badlee.get("id"));
+  }
+  _onClickComment() {
+    this.props.onClickComment(this.props.badlee.get("id"));
+  }
+  _onClickDelete() {
+    this.props.onClickDelete(this.props.badlee.get("id"));
+  }
   render() {
     let cardData = this.props.badlee.toJS();
     console.log(cardData);
@@ -44,7 +84,9 @@ class SingleBadlee extends Component {
         <Container style={{ flex: 1 }}>
           <Header style={{ backgroundColor: "#fff" }}>
             <Left>
-              <IconX name="arrow-back" />
+              <TouchableOpacity onPress={this.onBackPress}>
+                <Icon name="menuBackIcon" width="21" height="21" />
+              </TouchableOpacity>
             </Left>
             <Right>
               {cardData.purpose === "showOff" && (
@@ -61,48 +103,79 @@ class SingleBadlee extends Component {
           <Content style={{ flex: 1 }} contentContainerStyle={{ flex: 1 }}>
             <Card style={styles.card}>
               <CardItem header>
-                <TouchableOpacity
-                  style={{ flexDirection: "row", alignItems: "center" }}
-                >
-                  <Thumbnail
-                    source={{
-                      uri: cardData.user_info.avatar
-                    }}
-                    style={styles.userAvatar}
-                  />
-                  <Text style={styles.userName}>
-                    {cardData.user_info.username}
-                  </Text>
-                </TouchableOpacity>
-                <Text style={{ fontSize: 14 }}>
-                  <Text>'s</Text>
-                  <Text style={{ fontWeight: "bold" }}> Thingy</Text>
-                </Text>
-                <Text style={{ color: "#616161", fontSize: 15 }}>
-                  {" "}
-                  {moment(cardData.timestamp).fromNow()}
-                </Text>
+                <Left style={{ flex: 1 }}>
+                  <TouchableOpacity
+                    style={{ flexDirection: "row", alignItems: "center" }}
+                    onPress={this.onClickUser}
+                  >
+                    <Thumbnail
+                      source={{
+                        uri: cardData.user_info.avatar
+                      }}
+                      style={styles.userAvatar}
+                    />
+                  </TouchableOpacity>
+                  <View>
+                    <Text>
+                      <Text style={styles.userName} onPress={this.onClickUser}>
+                        {cardData.user_info.username}
+                      </Text>
+                      <Text style={{ fontSize: 14 }}>
+                        <Text>'s</Text>
+                        <Text style={{ fontWeight: "bold", fontSize: 14 }}>
+                          {" "}
+                          Thingy
+                        </Text>
+                      </Text>
+                      <Text style={{ color: "#616161", fontSize: 14 }}>
+                        {" "}
+                        {moment(cardData.timestamp).fromNow()}
+                      </Text>
+                    </Text>
+                    <View
+                      style={{ flexDirection: "row", alignItems: "center" }}
+                    >
+                      <Icon
+                        name="location"
+                        width="14"
+                        height="14"
+                        fill="#4c6a36"
+                      />
+                      <Text
+                        style={{
+                          color: "#616161",
+                          fontSize: 12,
+                          marginLeft: 2
+                        }}
+                      >
+                        {cardData.location}
+                      </Text>
+                    </View>
+                  </View>
+                </Left>
               </CardItem>
-              <CardItem cardBody style={{ flexDirection: "column" }}>
-                <Image
-                  source={{ uri: cardData.media }}
-                  style={styles.badleeImage}
-                />
+              <CardItem cardBody>
                 <Body>
-                  <Text style={styles.badleeLocation}>{cardData.location}</Text>
+                  <Image
+                    source={{ uri: cardData.media }}
+                    style={styles.badleeImage}
+                  />
                   <Text style={styles.badleeDescription}>
                     {cardData.description}
                   </Text>
                 </Body>
               </CardItem>
-              <CardItem footer style={{ marginBottom: 12 }}>
+              <CardItem footer>
                 <Left
                   style={{ flexDirection: "column", alignItems: "flex-start" }}
                 >
                   <View style={{ flexDirection: "row" }}>
                     {cardData.likes &&
                     cardData.likes.indexOf(this.props.userId) > -1 && (
-                      <TouchableOpacity transparent>
+                      <TouchableOpacity
+                        transparent
+                        onPress={this.onClickUnlike}
+                      >
                         <Icon
                           name="postLiked"
                           width="30"
@@ -115,7 +188,7 @@ class SingleBadlee extends Component {
                     {(!cardData.likes ||
                       (cardData.likes &&
                         cardData.likes.indexOf(this.props.userId) === -1)) && (
-                      <TouchableOpacity transparent>
+                      <TouchableOpacity transparent onPress={this.onClickLike}>
                         <Icon
                           name="postUnliked"
                           width="30"
@@ -129,7 +202,10 @@ class SingleBadlee extends Component {
                     )}
                     {cardData.wishes &&
                     cardData.wishes.indexOf(this.props.userId) > -1 && (
-                      <TouchableOpacity transparent>
+                      <TouchableOpacity
+                        transparent
+                        onPress={this.onClickUnwish}
+                      >
                         <Icon
                           name="postWished"
                           width="30"
@@ -142,7 +218,7 @@ class SingleBadlee extends Component {
                     {(!cardData.wishes ||
                       (cardData.wishes &&
                         cardData.wishes.indexOf(this.props.userId) === -1)) && (
-                      <TouchableOpacity transparent>
+                      <TouchableOpacity transparent onPress={this.onClickWish}>
                         <Icon
                           name="postUnwished"
                           width="30"
@@ -154,7 +230,7 @@ class SingleBadlee extends Component {
                         />
                       </TouchableOpacity>
                     )}
-                    <TouchableOpacity transparent>
+                    <TouchableOpacity transparent onPress={this.onClickComment}>
                       <Icon
                         name="postComment"
                         width="30"
@@ -172,22 +248,50 @@ class SingleBadlee extends Component {
                     <TouchableOpacity transparent>
                       <Text
                         style={{
-                          fontSize: 12,
+                          fontSize: 14,
                           marginLeft: 4,
                           fontWeight: "bold",
-                          color: "rgba(0, 0, 0, 0.87)"
+                          color: "rgba(0, 0, 0, 0.73)"
                         }}
                       >
-                        View {cardData.comment_count} comments{" "}
+                        {(cardData.wishes ? cardData.wishes.length : 0) +
+                          (cardData.likes ? cardData.likes.length : 0)}{" "}
+                        reactions{" "}
+                      </Text>
+                    </TouchableOpacity>
+                  </View>
+                  <View
+                    style={{
+                      marginTop: 3
+                    }}
+                  >
+                    <TouchableOpacity transparent onPress={this.onClickComment}>
+                      <Text
+                        style={{
+                          fontSize: 14,
+                          marginLeft: 4,
+                          fontWeight: "bold",
+                          color: "rgba(0, 0, 0, 0.73)"
+                        }}
+                      >
+                        {cardData.comment_count ? (
+                          `View ${cardData.comment_count}`
+                        ) : (
+                          0
+                        )}{" "}
+                        comments{" "}
                       </Text>
                     </TouchableOpacity>
                   </View>
                 </Left>
                 <Right>
-                  <TouchableOpacity transparent>
+                  <TouchableOpacity transparent onPress={this.onClickDelete}>
                     {cardData.user === this.props.userId && (
                       <Icon name="postDelete" width="30" height="30" />
                     )}
+                  </TouchableOpacity>
+                  <TouchableOpacity transparent onPress={this.onClickReport}>
+                    {cardData.user !== this.props.userId && <Text>report</Text>}
                   </TouchableOpacity>
                 </Right>
               </CardItem>
@@ -219,8 +323,8 @@ var styles = {
     width: 36,
     height: 36
   },
-  userAvatar: { height: 32, width: 32, marginLeft: 12, marginRight: 3 },
-  userName: { fontWeight: "bold", fontSize: 15, color: "rgba(0, 0, 0, 0.87)" },
+  userAvatar: { height: 40, width: 40, marginRight: 6 },
+  userName: { fontWeight: "bold", fontSize: 14, color: "rgba(0, 0, 0, 0.87)" },
   badleeImage: { height: 200, width: "100%", zIndex: 1 },
   badleeLocation: {
     marginLeft: 12,
@@ -247,7 +351,8 @@ const _Wrapped = connect(
       "badlees",
       "data",
       state.getIn(["badlees", "currentShowing"])
-    ])
+    ]),
+    userId: state.getIn(["user", "loggedUserID"])
   }),
   actionCreators
 )(SingleBadlee);

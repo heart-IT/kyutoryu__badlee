3e85fa7407a9cf09b4831a5c3a82e1ce7a86c6f6
Rahul Garg <rahul_garg08@yahoo.com>
2017-10-04 21:25:09 +0530

    ui changes for comments screen mostly

--- src/containers/Authenticated/Comments.js
+++ src/containers/Authenticated/Comments.js
@@ -9,7 +9,6 @@
  * @author- heartit pirates
  */
 import {
-    Body,
     Button,
     Container,
     Content,
@@ -20,7 +19,6 @@ import {
     Item,
     Left,
     List,
-    ListItem,
     StyleProvider,
     Text,
     Thumbnail,
@@ -56,41 +54,109 @@ class Comment extends Component {
   }
   render() {
     var comments = this.props.comments ? this.props.comments.toJS() : [];
+    console.log(comments);
     var commentsText = comments.map(comment => {
       return (
-        <ListItem key={comment.user_id + comment.timestamp} avatar>
-          <Left>
-            <Thumbnail source={{ uri: comment.avatar }} />
-          </Left>
-          <Body>
-            <Text>{comment.fname}</Text>
-            <Text note>{comment.content}</Text>
+        <View
+          key={comment.user_id + comment.timestamp}
+          style={{ flexDirection: "row" }}
+        >
+          <View style={{ paddingLeft: 6, paddingRight: 6 }}>
+            <Thumbnail
+              source={{ uri: comment.avatar }}
+              style={{ width: 42, height: 42 }}
+            />
+          </View>
+          <View>
+            <Text
+              style={{
+                color: "rgba(0, 0, 0, 0.87)",
+                fontWeight: "bold",
+                fontSize: 17,
+                lineHeight: 30
+              }}
+            >
+              {comment.fname}
+            </Text>
+            <Text style={{ fontSize: 15, color: "rgba(0, 0, 0, 0.72)" }}>
+              {comment.content}
+            </Text>
             <View style={styles.icons}>
               <TouchableOpacity transparent>
                 <Icon
                   name="postUnliked"
-                  width="24"
-                  height="27"
+                  width="22"
+                  height="24"
                   fill="none"
                   stroke="#000"
                   strokeWidth="17"
                 />
               </TouchableOpacity>
-              <TouchableOpacity transparent>
-                <Icon name="postDelete" width="30" height="30" />
-              </TouchableOpacity>
+              <View
+                style={{
+                  width: 5,
+                  height: 5,
+                  backgroundColor: "#611265",
+                  borderRadius: 3,
+                  marginLeft: 2,
+                  marginRight: 2
+                }}
+              />
               <Text
                 style={{
                   fontWeight: "bold",
                   fontSize: 14,
-                  color: "rgba(0, 0, 0, 0.77)"
+                  color: "rgba(0, 0, 0, 0.77)",
+                  paddingLeft: 2,
+                  paddingRight: 2
+                }}
+              >
+                Reply
+              </Text>
+              <View
+                style={{
+                  width: 5,
+                  height: 5,
+                  backgroundColor: "#611265",
+                  borderRadius: 3,
+                  marginLeft: 2,
+                  marginRight: 2
+                }}
+              />
+              <TouchableOpacity transparent>
+                <Icon
+                  name="postDelete"
+                  width="25"
+                  height="25"
+                  fill="none"
+                  stroke="#000"
+                  strokeWidth="17"
+                />
+              </TouchableOpacity>
+              <View
+                style={{
+                  width: 5,
+                  height: 5,
+                  backgroundColor: "#611265",
+                  borderRadius: 3,
+                  marginLeft: 2,
+                  marginRight: 2
+                }}
+              />
+              <Text
+                style={{
+                  fontWeight: "bold",
+                  fontSize: 14,
+                  color: "rgba(0, 0, 0, 0.77)",
+                  paddingLeft: 2,
+                  paddingRight: 2
                 }}
               >
                 2 likes
               </Text>
             </View>
-          </Body>
-        </ListItem>
+          </View>
+        </View>
       );
     });
     return (
@@ -100,8 +166,14 @@ class Comment extends Component {
             <Left
               style={{ flex: 1, flexDirection: "row", alignItems: "center" }}
             >
-              <IconX name="arrow-back" />
-              <Text style={{ marginLeft: 12, fontWeight: "bold" }}>
+              <IconX name="arrow-back" stroke="rgba(0, 0, 0, 0.81)" />
+              <Text
+                style={{
+                  color: "rgba(0, 0, 0, 0.87)",
+                  marginLeft: 12,
+                  fontSize: 18
+                }}
+              >
                 Comments
               </Text>
             </Left>
@@ -128,14 +200,14 @@ class Comment extends Component {
                     placeholder="enter something here"
                     onChangeText={commentText => this.setState({ commentText })}
                     value={this.state.commentText}
+                    style={styles.commentsInputBox}
                   />
                 </Item>
                 <Button
-                  transparent
                   style={styles.commentEnterBtn}
                   onPress={this.postComment}
                 >
-                  <Text>Save</Text>
+                  <Text>Post</Text>
                 </Button>
               </Form>
             </View>
@@ -155,9 +227,29 @@ let styles = {
     marginTop: 21,
     marginBottom: 21
   },
-  commentInputWrapper: { height: 50, backgroundColor: "#eeeeee" },
-  commentForm: { flexDirection: "row", alignItems: "center" },
-  commentsInput: { flex: 1 },
+  commentInputWrapper: {
+    height: 60,
+    backgroundColor: "#eeeeee",
+    padding: 9,
+    paddingLeft: 0
+  },
+  commentForm: { flexDirection: "row", alignItems: "center", height: 42 },
+  commentsInput: {
+    flex: 1,
+    height: 42
+  },
+  commentsInputBox: {
+    backgroundColor: "#fff",
+    height: 40,
+    paddingLeft: 6,
+    borderRadius: 3
+  },
+  commentEnterBtn: {
+    marginLeft: 9,
+    height: 36,
+    borderRadius: 18,
+    marginTop: 3
+  },
   noComments: {
     flex: 1,
     display: "flex",
@@ -170,14 +262,26 @@ let styles = {
   },
   icons: {
     flexDirection: "row",
-    alignItems: "center"
+    alignItems: "center",
+    paddingLeft: 3,
+    marginTop: 3
+  },
+  dot: {
+    width: 10,
+    height: 10,
+    backgroundColor: "#611265"
   }
 };
 
 const _Wrapped = connect(
   state => ({
     loading: state.getIn(["application", "isLoading"]),
-    comments: state.getIn(["badlees", "data", "currentShowing", "comments"])
+    comments: state.getIn([
+      "badlees",
+      "data",
+      state.getIn(["badlees", "currentShowing"]),
+      "comments"
+    ])
   }),
   actionCreators
 )(Comment);
